7ebcca282ade4d4bb0ee03367fe78b0b059557ea
Antônio de Pádua <antonyejoao@gmail.com>
2025-05-29 04:19:46 -0300

    implementando lógica de detalhes dos produtos e criando interface para capturar dados da api

--- src/app/components/list-product/list-product.component.ts
+++ src/app/components/list-product/list-product.component.ts
@@ -2,29 +2,46 @@ import { Component, OnInit } from '@angular/core';
 import { ListProductServiceService } from '../../services/list-product-service.service';
 import { CommonModule } from '@angular/common';
 
+interface Produto {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
 @Component({
   selector: 'app-list-product',
   imports: [CommonModule],
   templateUrl: './list-product.component.html',
   styleUrl: './list-product.component.scss'
 })
-export class ListProductComponent implements OnInit{
-  items : any = [];
-  title : string = '';
-  price : number = 0;
-  detalhesExibidos : boolean = false;
-  
-  constructor (private listProductService : ListProductServiceService) {}
+export class ListProductComponent implements OnInit {
+  items: Produto[] = [];
+  detalhesExibidos: boolean = false;
+  idSelecionado: number | null = null;
 
-  ngOnInit() : void {
-    this.listProductService.getProducts().subscribe(products => {
-      this.items = products;
-      console.log(products)
+  constructor(private listProductService: ListProductServiceService) { }
+
+  ngOnInit(): void {
+    this.listProductService.getProducts().subscribe(data => {
+      this.items = data;
+      console.log(data)
     })
   }
 
-  exibirDetalhes() {
+
+
+  exibirDetalhes(id: number) {
+    this.idSelecionado = this.idSelecionado === id ? null : id;
     this.detalhesExibidos = !this.detalhesExibidos
-    console.log("clicado");
   }
+
+
 }
+
